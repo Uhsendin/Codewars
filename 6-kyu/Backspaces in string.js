d5536f648c7e1e3b8b8a7697211b50c90aec5603
Brandon Rodriguez <chilebr99@gmail.com>
2023-06-30 23:34:17 -0400

    Added 6-kyu solution
    Backspaces in string

---
+++ 6-kyu/Backspaces in string.js
@@ -0,0 +1,24 @@
+/* 
+Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+
+Your task is to process a string with "#" symbols.
+
+Examples
+"abc#d##c"      ==>  "ac"
+"abc##d######"  ==>  ""
+"#######"       ==>  ""
+""              ==>  ""
+*/
+
+function cleanString(s) {
+  const stack = [];
+  
+  for (let char of s) {
+    if (char == '#') {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+  }
+  return stack.join('');
+}
